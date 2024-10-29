9cf6801d966aa06d74cb8160fa2b161ff7c93626
Xin Tie <44702214+xtie97@users.noreply.github.com>
2024-10-28 21:17:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ import os
 import torch
 from monai.apps.auto3dseg import AutoRunner
 
-# specify the algorithm to run
-algo = "lasnet" 
+import os
+import shutil
+import torch
+from monai.apps.auto3dseg import AutoRunner
 
-runner = AutoRunner(input="./configs/hyper_parameters.yaml", algos=algo, work_dir=f"./train_{algo}", train=True, ensemble=False)
+# create the template path and specify the algorithm to run
+templates_path = "algorithm_templates"
+algo = "lasnet"
+os.makedirs(templates_path, exist_ok=True)
+shutil.copytree("scripts", f"algorithm_templates/{algo}/scripts", dirs_exist_ok=True)
+shutil.copytree("configs", f"algorithm_templates/{algo}/configs", dirs_exist_ok=True)
+shutil.copyfile("algo_object.pkl", f"algorithm_templates/{algo}/algo_object.pkl")
+
+# specify the path to save the code and models
+work_dir = f"./train_{algo}"
+
+runner = AutoRunner(
+    input=os.path.join(templates_path, algo, "configs", "hyper_parameters.yaml"),
+    algos=algo,
+    work_dir=work_dir,
+    templates_path_or_url=templates_path,
+    train=True,
+    ensemble=False,
+)
 
 # set the device information
 num_gpus = torch.cuda.device_count()
